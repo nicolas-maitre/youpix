142103bab476c7e20bfdf677bb0331e0231f490c
MAITRE Nicolas <Nicolas.MAITRE@cpnv.ch>
2020-02-25 09:21:27 +0100

    merge

--- resources/views/pictures/show.blade.php
+++ resources/views/pictures/show.blade.php
@@ -1,3 +1,8 @@
 <a href="{{route('pictures.index')}}">retour</a>
 <h1>{{$picture->title}}</h1>
+<form action="{{route('pictures.destroy', $picture)}}" method="post">
+    @csrf
+    @method('DELETE')
+    <button type="submit">Supprimer</button>
+</form>
 <div style="background-image: url({{route('pictures.show', $picture->id)}}); width: 100%; height: 80vh; background-size: contain; background-position: center; background-repeat: no-repeat"></div>
