1fee3f4a38bda2cdd0490ea488331ad9febcc534
MAITRE Nicolas <nicolas.maitre@cpnv.ch>
2020-03-03 08:27:35 +0100

    added ajaj call for images add

--- app/Http/Controllers/PictureController.php
+++ app/Http/Controllers/PictureController.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Str;
 use Storage;
 use App\Picture;
 use Illuminate\Http\Request;
 use App\Http\Requests\PictureStoreRequest;
-
+use Aws\S3\S3Client;
+use Aws\S3\PostObjectV4;
 class PictureController extends Controller
 {
     /**
@@ -16,7 +19,7 @@ class PictureController extends Controller
     public function index()
     {
         $pictures = Picture::all();
-        return view("pictures.index")->with(["pictures"=>$pictures]);
+        return view("pictures.index")->with(["pictures" => $pictures]);
     }
 
     /**
@@ -26,8 +29,34 @@ class PictureController extends Controller
      */
     public function create()
     {
-        //
-        return view("pictures.create");
+        $client = new S3Client([
+            'version' => 'latest',
+            'region' => env('AWS_DEFAULT_REGION'),
+            'credentials' => ['key'    => env('AWS_ACCESS_KEY_ID'), 'secret' => env('AWS_SECRET_ACCESS_KEY'),],
+        ]);
+
+        $bucket = env('AWS_BUCKET');
+
+        $key = 'picturesNM/'.Str::random(40);
+        $formInputs = [
+            'acl' => 'private', 
+            'key' => $key
+        ];
+
+        $options = [
+            ['acl' => 'private'], 
+            ['bucket' => $bucket], 
+            ['eq', '$key', $key]
+        ];
+
+        $expires = '+5 minutes';
+
+        $postObject = new PostObjectV4($client, $bucket, $formInputs, $options, $expires);
+
+        $formAttributes = $postObject->getFormAttributes();
+
+        $formInputs = $postObject->getFormInputs();
+        return view("pictures.create", compact("formAttributes", "formInputs"));
     }
 
     /**
@@ -40,9 +69,18 @@ class PictureController extends Controller
     {
         $picture = new Picture();
         $picture->fill($request->all());
-        $picture->storage_path=$request->picture->store('pictures', 's3');
+        $picture->storage_path = $request->picture->store('pictures', 's3');
         $picture->save();
         return redirect()->route("pictures.show", compact('picture'));
+    }
+
+    public function apiStore(Request $request)
+    {
+        $picture = new Picture();
+        $picture->title = $request->title;
+        $picture->storage_path = $request->storage_path;
+        $picture->save();
+        return $picture;   
     }
 
     /**
@@ -53,7 +91,7 @@ class PictureController extends Controller
      */
     public function show(Request $request, Picture $picture)
     {
-        if(Str::startsWith ($request->header("Accept"), "image")){
+        if (Str::startsWith($request->header("Accept"), "image")) {
             return redirect(Storage::disk('s3')->temporaryUrl($picture->storage_path, time() + 60));
         }
         return view("pictures.show")->with(compact('picture'));
--- resources/views/pictures/create.blade.php
+++ resources/views/pictures/create.blade.php
@@ -1,14 +1,85 @@
 <html>
     <body>
         <a href="{{route('pictures.index')}}">retour</a>
-        <h1>Créer :)</h1>
-        <form action="{{route('pictures.store')}}" method="post" enctype="multipart/form-data">
-            @csrf
+        <h1>Créer :)2</h1>
+        <form id="addForm">
             <input type="text" name="title" required/>
             {{$errors->first('title')}}<br/>
             <input type="file" name="picture" required/>
             {{$errors->first('picture')}}<br/>
             <button type="submit">Envoyer</button>
         </form>
+        
+        <script>
+            var formAttributes = {!! json_encode($formAttributes) !!};
+            var formInputs = {!! json_encode($formInputs) !!};
+            var storeRoute = "{{route('pictureApiStore')}}";
+
+            
+            addForm.addEventListener("submit", onSubmit)
+            async function onSubmit(evt){
+                evt.preventDefault();
+                //get
+                var title = addForm.querySelector("[name=title]").value;
+                var picture = addForm.querySelector("[name=picture]").files[0];
+                //store
+                var isOk = await storeImage(picture);
+                if(!isOk){
+                    console.error("image store error", {isOk});
+                    alert("an error ocurred in image store");
+                    return;
+                }
+
+                var picture = await storePicture(title);
+                if(!picture){
+                    console.error("picture store error", {picture});
+                    alert("an error ocurred in picture store");
+                    return;
+                }
+                //redirect
+                window.location = `/pictures/${picture.id}`;
+            }
+            async function storeImage(picture){
+                const formData = new FormData();
+                for(var indInput in formInputs){
+                    formData.append(indInput, formInputs[indInput]);
+                }
+                formData.append("file", picture);
+
+                var fetchOptions = {
+                    method: formAttributes.method,
+                    headers: {
+                        //'Content-Type': formAttributes.enctype
+                    },
+                    body: formData
+                }
+                const res = await fetch(formAttributes.action, fetchOptions);
+                console.log("aaaaaaaaaaa", res);
+                if(!res.ok){
+                    console.log("error fetch", res);
+                    return false;
+                }
+                return true;
+            }
+            async function storePicture(title){
+                const formData = new FormData();
+                formData.append("title", title);
+                formData.append("storage_path", formInputs.key);
+
+                var fetchOptions = {
+                    method: "POST",
+                    headers: {
+                        //'Content-Type': formAttributes.enctype
+                    },
+                    body: formData
+                }
+                var res = await fetch(storeRoute, fetchOptions);
+                if(!res.ok){
+                    return false;
+                }
+                var decoded = await res.json();
+                return decoded;
+            }
+        </script>
     </body>
 </html>
